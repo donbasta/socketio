b3e8fd90afd7918c8d4da699ba45e76ea14e1244
donbasta <faddilafarras@gmail.com>
2021-04-24 22:37:40 +0700

    add event chat message

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () => {
